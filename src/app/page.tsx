f3f29ceed6c19dc508d8ab259db56c5f89b3a5c4
Aurelio Borges <aurelioaborges@gmail.com>
2023-08-19 15:20:06 -0300

    MTRA 02: update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,46 @@
+'use client'
+
+import { useState } from 'react'
 import styles from './page.module.scss'
-import { MdDone } from "react-icons/md"
+import Header from '@/components/Header'
+import Input from '@/components/Input'
+import Footer from '@/components/Footer'
+import TodoList from '@/components/TodoList'
 
 export default function Home() {
+  const [todos, setTodos] = useState<string[]>([])
+
+  const addTodo = (todo: string) => {
+    setTodos([...todos, todo])
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          This is my&nbsp;
-          <code className={styles.code}>Todo App</code>
-          <MdDone />
-        </p>
-      </div>
-    </main>
+    <div className={styles.main}>
+      <main className={styles.main_container}>
+        <div className={styles.main_header}>
+          <Header text={'Todo List'} />
+          <div className={styles.input_container}>
+            <Input
+              type={'text'}
+              name={'input-item'}
+              id={'input-item'}
+              placeholder={'Type here'}
+              addTodo={addTodo}
+            />
+          </div>
+        </div>
+
+        <div className={styles.main_header}>
+          {
+            todos.length === 0 ? '' :
+            <Header text={'Done'} />
+          }
+          <TodoList todos={todos} />
+        </div>
+
+      </main>
+
+      <Footer />
+    </div>
   )
 }
